039f01af4e30489eea24a2eb6f67134735f91dff
ramsdale93 <79873751+ramsdale93@users.noreply.github.com>
2021-03-07 13:13:17 +0000

    Update scorecards_scraping.py
    Updated to create a method

--- scorecards_scraping.py
+++ scorecards_scraping.py
@@ -13,47 +13,55 @@ from bs4 import BeautifulSoup
 
 #parse html to soup to be interpreted
 
-URL = 'https://www.cricketstats.org.uk/wdcu/2019/'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find_all('td',{'class':'fixtures_result'})
-dates = soup.find_all('td',{'class':'fixtures_date'})
+crick_stat = 'https://www.cricketstats.org.uk/'
 
 
-#save html to file
-html_file = open('year_results.txt','w')
-html_file.write(str(results))
-html_file.close()
-#print(dates)
+
+
 
 # get match and scorecard address for each match with a result
 base_url = 'https://www.cricketstats.org.uk/wdcu/2019/'
 df = pd.DataFrame()
 tb_columns = ['home', 'away', 'scorecard_link', 'reduced_overs']
 parsed_data = []
-for result in results:
-    fixture = result.find('b')
-    scorecard = result.find('a')
-    # if there was no result or match cancelled, skip the records
-    if None in (fixture, scorecard):
-        continue
-    match = fixture.text.strip()
-    #split match details, 1st team is home, 2nd team is away
-    teams = match.split(" v ")
-    home = teams[0]
-    away = teams[1]
-    overs = away.split("(")
-    away = overs[0]
-    if len(overs) == 2:
-        over = int((overs[1].split(" ",1))[0])
-    else:
-        over = None
-    index = scorecard.get('href')[30:72]
-    link = base_url + index
-    parsed_data.append([home, away, link, over])
+
+def scrape_homepage(league, year, save):
+    url = crick_stat  + league + '/' + year
+    print(url)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find_all('td',{'class':'fixtures_result'})
+    #dates = soup.find_all('td',{'class':'fixtures_date'})
+    for result in results:
+        fixture = result.find('b')
+        scorecard = result.find('a')
+        # if there was no result or match cancelled, skip the records
+        if None in (fixture, scorecard):
+            continue
+        match = fixture.text.strip()
+        #split match details, 1st team is home, 2nd team is away
+        teams = match.split(" v ")
+        home = teams[0]
+        away = teams[1]
+        overs = away.split("(")
+        away = overs[0]
+        if len(overs) == 2:
+            over = int((overs[1].split(" ",1))[0])
+        else:
+            over = None
+        index = scorecard.get('href')[30:72]
+        link = base_url + index
+        parsed_data.append([home, away, link, over])
+        
+    df=pd.DataFrame(parsed_data, columns = tb_columns)
+    if save == True:
+        #save html to file
+        html_file = open('year_results.txt','w')
+        html_file.write(str(results))
+        html_file.close()
+        #print(dates)
+        df.to_csv('teams.csv')
     
-df=pd.DataFrame(parsed_data, columns = tb_columns)
+    return df
 
-df.to_csv('teams.csv')
-
-
+match_list = scrape_homepage('wdcu', '2019', False)
