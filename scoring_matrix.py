1c53319fe00a47704c763a70731cbc4df08d415b
Jacob Stafford <jacob.stafford1993@gmail.com>
2021-03-14 19:02:52 +0000

    final 1s df being returned

---
+++ scoring_matrix.py
@@ -0,0 +1,9 @@
+scoring_matrix = {"run scored": 1,
+                  "50 scored": 10,
+                  "100 scored": 25,
+                  "wicket": 15,
+                  "3 for": 10,
+                  "5 for": 20,
+                  "maiden": 5,
+                  "duck": -5,
+                  "win": 10}
